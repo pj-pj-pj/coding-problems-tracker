138d26524e781c8dd3eb748c4b4a4edece2ba00b
Paula Joyce Ucol <peaksenut@gmail.com>
2025-04-15 22:14:43 +0800

    feat: add dialog to view full problem details

--- components/problem-item.tsx
+++ components/problem-item.tsx
@@ -1,19 +1,13 @@
 "use client";
 
-import { createClient } from "@/utils/supabase/client";
-import { useEffect, useState } from "react";
-import { SubmitButton } from "./submit-button";
+import ProblemDialog from "./problem-dialog";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 
 export default function ProblemItem({ problem }: any) {
-  const [problems, setProblems] = useState<any[] | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-  const supabase = createClient();
-
   return (
-    <li className="text-xs bg-card hover:bg-orange-200 p-4 border border-secondary w-full flex-1/2 space-y-2">
-      <span className="flex gap-3 font-semibold items-center mb-1 max-w-[420px]">
-        <h2 className="text-sm truncate">{problem.title}</h2>
+    <li className="text-xs bg-card hover:bg-orange-200 p-4 border border-secondary w-full flex-1/2 space-y-1">
+      <span className="flex text-sm gap-3 font-semibold items-center mb-3 max-w-[480px]">
+        <h2 className="truncate">{problem.title}</h2>
         <p
           className={
             problem.status === "Solved"
@@ -26,17 +20,19 @@ export default function ProblemItem({ problem }: any) {
           [{problem.status}]
         </p>
       </span>
-      <span className="flex gap-2 items-center">
-        <p className="text-nowrap">Difficulty:</p>
+      <span className="flex items-center">
+        -{"  "}
+        <p className="pl-4 pr-3 text-nowrap">Difficulty:</p>
         {problem.difficulty}
       </span>
       {problem.problem_url && (
         <span className="flex items-center">
-          <p className="text-nowrap">Problem URL:</p>
+          -{"  "}
+          <p className="pl-4 pr-1 text-nowrap">Problem URL:</p>
           <a
             href={problem.problem_url}
             target="blank"
-            className="truncate px-2 hover:bg-secondary hover:underline hover:text-primary-foreground"
+            className="truncate px-2 hover:underline text-violet-700"
           >
             {problem.problem_url}
           </a>
@@ -44,19 +40,26 @@ export default function ProblemItem({ problem }: any) {
       )}
       {problem.solution_url && (
         <span className="flex items-center">
-          <p className="text-nowrap">Solution URL:</p>
+          -{"  "}
+          <p className="pl-4 pr-1 text-nowrap">Solution URL:</p>
           <a
             href={problem.solution_url}
             target="blank"
-            className="truncate px-2 hover:bg-secondary hover:underline hover:text-primary-foreground"
+            className="truncate px-2 hover:underline text-violet-700"
           >
             {problem.solution_url}
           </a>
         </span>
       )}
-      <SubmitButton className="h-8 hover:bg-transparent hover:underline bg-transparent text-secondary font-semibold p-0 text-xs">
-        View Problem
-      </SubmitButton>
+      <div className="h-2 text-background">-</div>
+      <Dialog>
+        <DialogTrigger className="hover:bg-secondary hover:text-primary-foreground underline bg-transparent text-secondary font-semibold p-0 text-xs hover:px-2">
+          View Problem
+        </DialogTrigger>
+        <DialogContent>
+          <ProblemDialog problem={problem} />
+        </DialogContent>
+      </Dialog>
     </li>
   );
 }
