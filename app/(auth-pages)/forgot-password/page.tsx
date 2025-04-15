fc5da181e181ed5b20e782757e735475f7dcf8d9
Paula Joyce Ucol <peaksenut@gmail.com>
2025-04-15 10:21:24 +0800

    style; forgot password to look like reset password (violet)

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -3,8 +3,8 @@ import { FormMessage, Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { ChevronDown } from "lucide-react";
 import Link from "next/link";
-import { SmtpMessage } from "../smtp-message";
 
 export default async function ForgotPassword(props: {
   searchParams: Promise<Message>;
@@ -14,24 +14,41 @@ export default async function ForgotPassword(props: {
     <>
       <form className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64 mx-auto">
         <div>
-          <h1 className="text-2xl font-medium">Reset Password</h1>
-          <p className="text-sm text-secondary-foreground">
+          <span className="flex items-center gap-3 text-violet-500 py-1 pb-2">
+            <ChevronDown
+              size={20}
+              strokeWidth={3}
+              className="self-center"
+            />
+            <h1 className="text-base font-bold">Reset Password</h1>
+          </span>
+          <p className="text-xs text-secondary-foreground">
             Already have an account?{" "}
-            <Link className="text-primary underline" href="/sign-in">
+            <Link
+              className="text-primary underline hover:bg-primary hover:text-background px-2"
+              href="/sign-in"
+            >
               Sign in
             </Link>
           </p>
         </div>
         <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
           <Label htmlFor="email">Email</Label>
-          <Input name="email" placeholder="you@example.com" required />
-          <SubmitButton formAction={forgotPasswordAction}>
-            Reset Password
+          <Input
+            name="email"
+            placeholder="you@example.com"
+            className="border-violet-500"
+            required
+          />
+          <SubmitButton
+            className="bg-violet-500 hover:text-violet-500 hover:bg-violet-200 hover:border hover:border-violet-500"
+            formAction={forgotPasswordAction}
+          >
+            Reset password
           </SubmitButton>
           <FormMessage message={searchParams} />
         </div>
       </form>
-      <SmtpMessage />
     </>
   );
 }
